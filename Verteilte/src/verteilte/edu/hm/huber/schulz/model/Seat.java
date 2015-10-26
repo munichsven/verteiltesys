d3d603dd360ec31ddc6e536601da304d6c025fe5
huber49 <huber49@hm.edu>
2015-10-26 11:11:56 +0100

    neuer branch

--- Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
+++ Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
@@ -2,7 +2,7 @@ package verteilte.edu.hm.huber.schulz.model;
 
 public class Seat {
 		private final int id;
-		private int test;
+		private int test2;
 		public Seat(final int id){
 			this.id = id;
 		}
