5eacda3bb7e2dbdfe28f41c05df2d1ff41195113
Sven Schulz <sschulz@hm.edu>
2015-10-26 12:32:03 +0100

    Controller und Starter

--- Verteilte/src/verteilte/edu/hm/huber/schulz/controller/Controller.java
+++ Verteilte/src/verteilte/edu/hm/huber/schulz/controller/Controller.java
@@ -1,4 +1,11 @@
 package verteilte.edu.hm.huber.schulz.controller;
+
+import java.util.ArrayList;
+
+import verteilte.edu.hm.huber.schulz.model.MasterOfSeat;
+import verteilte.edu.hm.huber.schulz.model.Philosoph;
+import verteilte.edu.hm.huber.schulz.model.Seat;
+
 /**
  * 
  * @author Zwen
@@ -6,9 +13,52 @@ package verteilte.edu.hm.huber.schulz.controller;
  */
 public class Controller {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private ArrayList<Seat> seatList;
+	private ArrayList<MasterOfSeat> masterList;
+	private ArrayList<Philosoph> philosphenList;
+	private final int seat;
+	private final int philosoph;
+	private final int hungryPeople;
+	
+	public Controller(final int seat, final int philosoph, final int hungryPeople){
+		this.seat = seat;
+		this.philosoph = philosoph;
+		this.hungryPeople = hungryPeople;
+		
+		masterList = new ArrayList<MasterOfSeat>();
+		philosphenList = new ArrayList<Philosoph>();
+		seatList = new ArrayList<Seat>();
+		
+		for(int i = 0 ; i < seat; i++){
+			Seat newSeat = new Seat(i+1);
+			MasterOfSeat master = new MasterOfSeat(newSeat);
+			masterList.add(master);
+			seatList.add(newSeat);
+		}
+		
+		for(int i = 0; i < philosoph;i++){
+			Philosoph phil = new Philosoph(false, i+1);
+			philosphenList.add(phil);
+		}
+	}
+	
+	public ArrayList<Seat> getSeatList() {
+		return seatList;
+	}
+	public void setSeatList(ArrayList<Seat> seatList) {
+		this.seatList = seatList;
+	}
+	public ArrayList<MasterOfSeat> getMasterList() {
+		return masterList;
+	}
+	public void setMasterList(ArrayList<MasterOfSeat> masterList) {
+		this.masterList = masterList;
+	}
+	public ArrayList<Philosoph> getPhilosphenList() {
+		return philosphenList;
+	}
+	public void setPhilosphenList(ArrayList<Philosoph> philosphenList) {
+		this.philosphenList = philosphenList;
 	}
 
 }
---
+++ Verteilte/src/verteilte/edu/hm/huber/schulz/controller/Starter.java
@@ -0,0 +1,10 @@
+package verteilte.edu.hm.huber.schulz.controller;
+
+public class Starter {
+	
+	public static void main(String args[]){
+		
+		Controller controller = new Controller(1, 2, 0);
+	}
+
+}
