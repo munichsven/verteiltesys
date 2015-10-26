cc42cdfb9a5dbf2093d46cf7b7dbcb336137de9f
huber49 <huber49@hm.edu>
2015-10-26 11:09:39 +0100

    test

--- Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
+++ Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
@@ -2,7 +2,7 @@ package verteilte.edu.hm.huber.schulz.model;
 
 public class Seat {
 		private final int id;
-		private int test;
+		private int tes;
 		public Seat(final int id){
 			this.id = id;
 		}
