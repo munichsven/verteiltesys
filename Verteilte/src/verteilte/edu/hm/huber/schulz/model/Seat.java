d4329e8f75eb16ef003a7c8b5a93b51d606f903e
huber49 <huber49@hm.edu>
2015-10-26 11:21:34 +0100

    Seat angepasst

--- Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
+++ Verteilte/src/verteilte/edu/hm/huber/schulz/model/Seat.java
@@ -1,12 +1,26 @@
 package verteilte.edu.hm.huber.schulz.model;
 
 public class Seat {
-		private final int id;
-		private int test;
-		public Seat(final int id){
-			this.id = id;
-		}
-		public int getId() {
-			return id;
-		}
+
+	private final int id;
+
+	//gibt an, ob Platz besetzt ist
+	private boolean inUse;
+
+	public Seat(final int id) {
+		this.id = id;
+		this.inUse = false;
+	}
+
+	public boolean isInUse() {
+		return inUse;
+	}
+
+	public void setInUse(boolean inUse) {
+		this.inUse = inUse;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
